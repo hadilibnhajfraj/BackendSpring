f3e07fcf67e44acd3148bd8fc4f8047e329bef48
amina333 <athmouni.amina@esprit.tn>
2025-04-11 18:22:04 +0100

    Resolved merge conflicts

---
+++ src/main/java/tn/esprit/spring/entities/Commentaire.java
@@ -0,0 +1,32 @@
+package tn.esprit.spring.entities;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Commentaire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String texte;
+    private LocalDate dateCommentaire;
+
+    @ManyToOne
+    //@JoinColumn(name = "id_user")
+    private User user;
+
+    @ManyToOne
+    //@JoinColumn(name = "id_publication")
+    private Publication publication;
+
+
+}
---
+++ src/main/java/tn/esprit/spring/entities/Terrain.java
@@ -0,0 +1,36 @@
+package tn.esprit.spring.entities;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Terrain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    private String logo;
+    private String adresse;
+    private int nbSpectateur;
+    private int prix;
+
+    @OneToMany(mappedBy = "terrain", cascade = CascadeType.ALL)
+    List<MatchFo> matchFos;
+
+    @OneToMany(mappedBy = "terrain", cascade = CascadeType.ALL)
+    List<Reservation> reservations;
+
+    @OneToMany(mappedBy = "terrain", cascade = CascadeType.ALL)
+    private List<Disponibilite_terrain> disponibilites; // Liste des créneaux
+
+    @ManyToOne
+    private User proprietaire;
+}
