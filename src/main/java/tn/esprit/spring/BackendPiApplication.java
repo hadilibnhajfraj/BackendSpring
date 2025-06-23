b12a9c44f814900eeff06ec7c19a01934cfbdec3
amina333 <athmouni.amina@esprit.tn>
2025-06-23 20:09:28 +0100

    commit avec sonar

--- src/main/java/tn/esprit/spring/BackendPiApplication.java
+++ src/main/java/tn/esprit/spring/BackendPiApplication.java
@@ -1,9 +1,8 @@
-package tn.esprit.spring;
+package main.java.tn.esprit.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class BackendPiApplication {
 
     public static void main(String[] args) {
