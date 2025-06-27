bf8384708ba9213b43a74dd588cf8114e42a8eb2
alawardi7033 <ouardi.alaeddinne@esprit.tn>
2025-06-27 17:05:14 +0200

    modif confd

--- src/main/java/tn/esprit/spring/BackendPiApplication.java
+++ src/main/java/tn/esprit/spring/BackendPiApplication.java
@@ -2,14 +2,14 @@ package tn.esprit.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
-@EnableScheduling
 public class BackendPiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendPiApplication.class, args);
     }
+
 
 }
