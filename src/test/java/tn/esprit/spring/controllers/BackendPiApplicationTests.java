3d39a76f367d4a5a4b411d786fb493fc314967a0
alawardi7033 <ouardi.alaeddinne@esprit.tn>
2025-06-28 23:16:02 +0200

    test 29

---
+++ src/test/java/tn/esprit/spring/controllers/BackendPiApplicationTests.java
@@ -0,0 +1,17 @@
+package tn.esprit.spring.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class BackendPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
