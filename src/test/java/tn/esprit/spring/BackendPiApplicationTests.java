1a04d6dc16fbb9cd5fb75625b39d67b215b5bd6b
MeriemBenSalem <meriem.bsm.fb@gmail.com>
2025-06-28 14:11:39 +0200

    config test

--- src/test/java/tn/esprit/spring/BackendPiApplicationTests.java
+++ src/test/java/tn/esprit/spring/BackendPiApplicationTests.java
@@ -3,7 +3,13 @@ package tn.esprit.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendPiApplicationTests {
 
     @Test
